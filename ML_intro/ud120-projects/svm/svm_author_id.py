cc3c91c4cea6a7d8d4f95c94342891f9c95cc060
aadiuppal <aadiuppal@gmail.com>
2015-02-27 03:31:21 -0600

    old code added -4

--- ML_intro/ud120-projects/svm/svm_author_id.py
+++ ML_intro/ud120-projects/svm/svm_author_id.py
@@ -22,11 +22,33 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
 
 #########################################################
 ### your code goes here ###
-
+from sklearn import svm
+#c=[10,100,1000,10000,100000]
+c=[10000]
+count=0
+for i in c:
+	clf=svm.SVC(C=i,kernel="rbf")
+	t0=time()
+	clf.fit(features_train,labels_train)
+	print "training time:", round(time()-t0, 3), "s"
+	t1=time()
+	pred=clf.predict(features_test)
+	print "training time:", round(time()-t1, 3), "s"
+	from sklearn.metrics import accuracy_score
+	accuracy = accuracy_score(labels_test,pred)
+	print "for C=",i,"  accuracy=",accuracy
+	print "for 10",pred[10]
+	print "for 26",pred[26]
+	print "for 50",pred[50]
+	for i in pred:
+		if i==1:
+			count+=1
+	print count
 #########################################################
 
 
