0acd2bcefe0399925e76e507939932eb5523ec4e
aadiuppal <aadiuppal@gmail.com>
2015-03-12 15:07:47 -0500

    k means lesson

--- ML_intro/ud120-projects/k_means/k_means_cluster.py
+++ ML_intro/ud120-projects/k_means/k_means_cluster.py
@@ -49,24 +49,31 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
+feature_4 = "exercised_stock_options"#"salary"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2,feature_3]
+features=[feature_4]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
-
+featureArray = featureFormat(data_dict, features)
+f_max= numpy.nanmax(featureArray)
+f_min= numpy.nanmin(featureArray)
+value=1000000#200000
+print float(value-f_min)/(f_max-f_min)
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, f3 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 
 
 from sklearn.cluster import KMeans
-features_list = ["poi", feature_1, feature_2]
+features_list = ["poi", feature_1, feature_2,feature_3]
 data2 = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data2 )
 clf = KMeans(n_clusters=2)
